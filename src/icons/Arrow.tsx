e7f0a2a1c982119410e095671edce574951dd08d
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2023-12-27 14:36:57 +0700

    feat: adding icons: Arrow, Star

---
+++ src/icons/Arrow.tsx
@@ -0,0 +1,35 @@
+interface ArrowProps {
+  className?: string;
+}
+
+const Arrow = ({ className }: ArrowProps) => {
+  return (
+    <svg
+      className={className}
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.61987 3.95331L13.6665 7.99997L9.61987 12.0466"
+        stroke="#A8AEBF"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.33325 8H13.5533"
+        stroke="#A8AEBF"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Arrow;
