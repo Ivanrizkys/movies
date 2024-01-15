bfa09ae290616d8f32d8c0ebf389147c2df6167d
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-01-15 20:52:59 +0700

    feat: adding new icons: 'Close', 'Hearts', 'Search', and 'Thumb'

---
+++ src/icons/Close.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+const Close = () => {
+  return (
+    <svg
+      width={10}
+      height={10}
+      viewBox="0 0 10 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.757812 0.757355L9.24309 9.24261"
+        stroke="#8E95A9"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0.757812 9.24261L9.24312 0.757355"
+        stroke="#8E95A9"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Close;
