5773d501b89681a5b6c44490f09540d7c9ac2b3c
Ivan Rizky Saputra <ivnrzkys@gmail.com>
2024-01-15 21:00:15 +0700

    feat: add link for suggest-me page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,20 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full z-10 bg-[#121829]/50 backdrop-blur-lg">
-    <Container className="flex justify-between items-center py-4">
-      <Link href={"/"}>
+      <Container className="flex justify-between items-center py-4">
+        <Link href={"/"}>
           <Image src={"/logo.png"} width={28} height={28} alt="app-logo" />
-      </Link>
-      <button
-        className="text-[#A8AEBF] font-semibold text-base flex items-center gap-2"
-      >
-        <span>Contact</span>
-        <Arrow />
-      </button>
-    </Container>
-  </nav>
-  )
-}
+        </Link>
+        <Link
+          href={"/suggest-me"}
+          className="text-[#A8AEBF] font-semibold text-base flex items-center gap-2"
+        >
+          <span>Suggest me</span>
+          <Arrow />
+        </Link>
+      </Container>
+    </nav>
+  );
+};
 
 export default Navbar;
